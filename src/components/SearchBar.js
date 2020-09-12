8dedee5315b1b48613ef87bd6a57762888f424b1
Glyn Davies <glynwdavies@gmail.com>
2020-09-12 15:39:44 +0100

    Added alternate onChange syntax

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Picture Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        <input type="text" onChange={(event) => console.log(event.target.value)} />
                     </div>
                 </form>
             </div>
